d3df209740a444ef7fb17f546fa6bde1a48b5882
Ankit Sagu <ankitsagu007@gmail.com>
2017-05-04 18:44:06 +0000

    draw function written

--- fifteen/fifteen.c
+++ fifteen/fifteen.c
@@ -179,6 +179,17 @@ void init(void)
 void draw(void)
 {
     // TODO
+     int i,j;
+    for(i=0;i<d;i++){
+        for(j=0;j<d;j++){
+            if(board[i][j] == 0){
+                printf("  _");
+            }else{
+                printf(" %2i",board[i][j]);
+            }
+        }
+        printf("\n\n");
+    }
     
 }
 
